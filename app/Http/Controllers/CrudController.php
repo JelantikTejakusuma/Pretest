47dadc46d997c0addd36cc0b61385f2d3204ac7e
Tejakusuma <tejatop@gmail.com>
2022-12-08 21:00:10 +0800

    upload program

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use \Illuminate\Http\Response;
+use Yajra\DataTables\Facades\DataTables;
+
+class CrudController extends Controller
+{
+    public function index(){
+        $icon = 'ni ni-dashlite';
+        $subtitle = 'Mahasiswa';
+        $table_id = 'tbmahasiswa';
+        return view('crud',compact('subtitle','table_id','icon'));
+    }
+
+    public function listData(Request $request){
+        $data = Mahasiswa::all();
+        $datatables = DataTables::of($data);
+        return $datatables
+                ->addIndexColumn()
+                ->addColumn('aksi', function($data){
+                    $aksi = "";
+                    $aksi .= "<a title='Edit Data' href='/crud/".$data->id."/edit' class='btn btn-md btn-primary' data-toggle='tooltip' data-placement='bottom' onclick='buttonsmdisable(this)'><i class='ti-pencil' ></i></a>";
+                    $aksi .= "<a title='Delete Data' href='javascript:void(0)' onclick='deleteData(\"{$data->id}\",\"{$data->nim}\",this)' class='btn btn-md btn-danger' data-id='{$data->id}' data-nim='{$data->nim}'><i class='ti-trash' data-toggle='tooltip' data-placement='bottom' ></i></a> ";
+                    return $aksi;
+                })
+                ->rawColumns(['aksi'])
+                ->make(true);
+    }
+
+    public function deleteData(Request $request){
+        if(Mahasiswa::destroy($request->id)){
+            $response = array('success'=>1,'msg'=>'Berhasil hapus data');
+        }else{
+            $response = array('success'=>2,'msg'=>'Gagal menghapus data');
+        }
+        return $response;
+    }
+
+    public function create(){
+        $icon = 'ni ni-dashlite';
+        $subtitle = 'Tambah Data Mahasiswa';
+        return view('create',compact('subtitle','icon'));
+    }
+
+    public function edit(Request $request){
+        $data = Mahasiswa::find($request->id);
+        $icon = 'ni ni-dashlite';
+        $subtitle = 'Edit Data Mahasiswa';
+        return view('edit',compact('subtitle','icon','data'));
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'nim' => ['required','integer',],
+            'nama' => ['required',],
+            'alamat' => ['required',],
+        ]);
+        Mahasiswa::create($request->all());
+        return redirect('/crud');
+        //return $request;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'nim' => ['required','integer'],
+            'nama' => ['required'],
+            'alamat' => ['required'],
+        ]);
+        $mahasiswa = Mahasiswa::find($request->id);
+        $mahasiswa->fill($request->all())->save();
+        return redirect('/crud');
+    }
+}
